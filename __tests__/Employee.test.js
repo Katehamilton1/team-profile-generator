d785b8d92b66a1e59ba506f5c42c9da7d5ee1676
Kate Hamilton <kate.hamilton828@gmail.com>
2021-11-17 09:32:15 -0500

    employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,11 @@
 // using Employee constructor
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
 const Employee = require('../lib/Employee');
 
 // creates employee object 
 
-test ('creates an employee object', () => {
+test('creates an employee object', () => {
     const employee = new Employee();
 
     expect(employee.name).toEqual(expect.any(String));
@@ -11,3 +13,20 @@ test ('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
+test('Can set name via constructor', () => {
+    const name = '';
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+})
+
+test('can set id via constructor', () => {
+    const idValue='' ;
+    const employee = new Employee(idValue);
+    expect(employee.id).toBe(idValue);
+})
+
+test('can set email via constructor', () => {
+    const getEmail='' ;
+    const employee = new Employee(getEmail);
+    expect(employee.getEmail).toBe(getEmail);
+})
